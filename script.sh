fbdb639107a09b865c90443b376f0edf5aa02653
Dogneri <70638698+Dogneri@users.noreply.github.com>
2021-05-31 17:37:42 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ pr="$(echo $out | awk '{print $1}')";
 pl="$(echo $out | awk '{print $4}')";
 if [[ $pl -eq 0 ]]
 then
-   echo "Echec de connexion. Veuillez voir l'état la connexion ou de l'appareil" | mail -s "Etat de connectivité" karimesoro53@gmail.com
+   echo "Echec de connexion. Veuillez voir l'état la connexion ou de l'appareil" | mail -s "Etat de connectivité" your_mail_address
 fi
 if [[ pl -ne 0 ]]
 then
-  echo "Votre connexion est établie" | mail -s "Etat de connectivité" karimesoro53@gmail.com
+  echo "Votre connexion est établie" | mail -s "Etat de connectivité" your_mail_address
   
 fi
